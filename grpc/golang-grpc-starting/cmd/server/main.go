0c511515f647726e54befde18d56b41dffaa4545
dskst <10830352+dskst@users.noreply.github.com>
2023-07-22 23:45:38 +0900

    feat: add healthcheck

--- grpc/golang-grpc-starting/cmd/server/main.go
+++ grpc/golang-grpc-starting/cmd/server/main.go
@@ -5,6 +5,8 @@ import (
 	"errors"
 	"fmt"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/health"
+	healthpb "google.golang.org/grpc/health/grpc_health_v1"
 	"google.golang.org/grpc/metadata"
 	"google.golang.org/grpc/reflection"
 	"io"
@@ -33,6 +35,11 @@ func main() {
 	)
 
 	hellopb.RegisterGreetingServiceServer(s, NewMyServer())
+
+	healthSrv := health.NewServer()
+	healthpb.RegisterHealthServer(s, healthSrv)
+	healthSrv.SetServingStatus("mygrpc", healthpb.HealthCheckResponse_SERVING)
+	healthSrv.SetServingStatus("", healthpb.HealthCheckResponse_NOT_SERVING)
 
 	reflection.Register(s)
 
