109fa08c94e640c1f97dcebc14071662eca41219
dskst <10830352+dskst@users.noreply.github.com>
2025-03-26 15:50:36 +0900

    feat: add mcp

---
+++ modelcontextprotocol/server/src/test/java/modelcontextprotocol/sandbox/dskst/mcpserver/McpserverApplicationTests.java
@@ -0,0 +1,13 @@
+package modelcontextprotocol.sandbox.dskst.mcpserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McpserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
